99b9b2b0cb7f40f4326f4353bde8ccfa502bf8d8
Nicolai Vasquez <nrvasquez@gmail.com>
2017-04-14 21:11:43 +0100

    refactor sidebar to accept contents via props api class to fetch data add sidebar tests

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar';
 
-import { Sidebar } from './components/Sidebar';
+import { Api } from './data/api';
+import {Sidebar} from './components/Sidebar/Sidebar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tableOfContents: [],
+    };
+  }
+
+  componentDidMount() {
+    Api.fetchTableOfContents()
+      .then((tableOfContents) => {
+        this.setState({
+          tableOfContents,
+        })
+      });
+  }
+
   render() {
     return (
-        <MuiThemeProvider>
-            <div className="container">
-                <Toolbar className="header">
-                    <ToolbarGroup firstChild={true}>
-                        Test
-                    </ToolbarGroup>
-                </Toolbar>
-                <div className="main">
-                    <Sidebar />
-                    <div className="content">Main Content</div>
-                </div>
-            </div>
-        </MuiThemeProvider>
+      <MuiThemeProvider>
+        <div className="container">
+          <Toolbar className="header">
+            <ToolbarGroup firstChild={true}>
+              Test
+            </ToolbarGroup>
+          </Toolbar>
+          <div className="main">
+            <Sidebar contents={this.state.tableOfContents} />
+            <div className="content">Main Content</div>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
