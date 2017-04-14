658d8ceb30ebccbe6de91ff76a071d807b0777d6
Nicolai Vasquez <nrvasquez@gmail.com>
2017-04-14 21:31:24 +0100

    separate out ToC component from sidebar

--- src/components/Sidebar/Sidebar.test.js
+++ src/components/Sidebar/Sidebar.test.js
@@ -1,34 +1,10 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import {Sidebar} from './Sidebar';
+import { shallow } from 'enzyme';
+import { Sidebar } from './Sidebar';
 
 it('renders without crashing', () => {
   const sidebar = shallow(
     <Sidebar />
   );
   expect(sidebar.find('.sidebar').length).toEqual(1);
-});
-
-it('renders the correct number of list groups', () => {
-  const testContents = [
-    {label: 'Test'},
-    {label: 'Another'},
-  ];
-  const sidebar = shallow(
-    <Sidebar contents={testContents} />
-  );
-
-  expect(sidebar.find('ListGroup').length).toEqual(testContents.length);
-});
-
-it('renders the correct number of dividers', () => {
-  const testContents = [
-    {label: 'Test'},
-    {label: 'Another'},
-  ];
-  const sidebar = shallow(
-    <Sidebar contents={testContents} />
-  );
-
-  expect(sidebar.find('Divider').length).toEqual(testContents.length - 1);
 });
