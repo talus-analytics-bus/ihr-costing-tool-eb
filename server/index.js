825db9a32e3774d5f4729f21f7ec2523751035d9
Nicolai Vasquez <nrvasquez@gmail.com>
2017-04-19 22:20:07 +0100

    added mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,48 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const port = 9500;
 let app = express();
 
+mongoose.connect('mongodb://mongo:27017');
+
+let Chocolate;
+
+const testMongo = () => {
+  const chocolateSchema = mongoose.Schema({
+    name: String,
+    type: String,
+    bars: Number,
+    sugarFree: Boolean,
+  });
+
+  Chocolate = mongoose.model('Chocolate', chocolateSchema);
+
+  const KitKat = new Chocolate({
+    name: 'KitKat',
+    type: 'dark',
+    bars: 4,
+    sugarFree: false,
+  });
+
+  KitKat.save();
+};
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.error('connection error');
+});
+db.once('open', () => {
+  console.log('connected to mongo');
+
+  testMongo();
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  Chocolate.find((err, chocolates) => {
+    if (err) res.send('Hello, world');
+    res.send(chocolates);
+  });
 });
 
 app.listen(port, () => {
