b8dc6f014b9998c5144d81330cedbf905f7b42ed
Nicolai Vasquez <nrvasquez@gmail.com>
2017-04-23 21:12:28 +0100

    add fonts home styling

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+import styles from './Home.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router-dom'
+
 export class Home extends Component {
   render() {
     return (
-      <div className="home">
-        Home Tab
+      <div className={styles.home}>
+        <div className={[styles.homeCenterContents, styles.homeSection].join(' ')}>
+          <h1>Welcome to the IHR Costing Tool</h1>
+        </div>
+
+        <div className={styles.homeSection}>
+          <p>
+            The International Health Regulations (IHR) Costing Tool is a tool intended to evaluate the costs for a country
+            to upgrade its capacity to prevent, detect and rapidly respond to public health threats. The tool is based on
+            the Joint External Evaluation (JEE) tool.
+          </p>
+        </div>
+
+        <div className={[styles.homeSection, styles.homeCenterContents].join(' ')}>
+          <RaisedButton
+            label="Enter"
+            containerElement={<Link to="/identification/" />}
+            linkButton={true}
+          />
+        </div>
       </div>
     )
   }
