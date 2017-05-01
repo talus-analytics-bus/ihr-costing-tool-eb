743c971fb0dbc124e7d10d8570afede991b74a71
Nicolai Vasquez <nrvasquez@gmail.com>
2017-05-01 13:24:45 +0100

    populate table of contents

--- src/scenes/Assessment/Assessment.js
+++ src/scenes/Assessment/Assessment.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { Api } from '../../data/api';
+import { Sidebar} from '../../components/Sidebar/Sidebar';
+import { TableOfContentsActive } from './components/TableOfContents/TableOfContentsActive';
 
 import styles from './Assessment.css';
 
@@ -9,7 +11,16 @@ export class Assessment extends Component {
     if (this.props.jeeTree !== []) {
       Api.fetchJeeTree()
         .then((jeeTree) => {
-          this.props.populateJeeTree(jeeTree);
+          this.props.populateJeeTree(jeeTree.map((indicator) => ({
+            ...indicator,
+            active: false,
+            completed: false,
+            capacities: (indicator.capacities || []).map((capacity) => ({
+              ...capacity,
+              active: false,
+              completed: false,
+            }))
+          })));
         });
     }
   }
@@ -17,6 +28,12 @@ export class Assessment extends Component {
   render() {
     return (
       <div className={styles.assessment}>
+        <div className={styles.wrapper}>
+          <Sidebar>
+            <TableOfContentsActive/>
+          </Sidebar>
+          <div>Hello</div>
+        </div>
       </div>
     )
   }
---
+++ src/scenes/Assessment/components/TableOfContents/TableOfContents.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import styles from './TableOfContents.css';
+
+class Capacity extends Component {
+  render() {
+    return (
+      <div className={styles.capacity}>
+        {this.props.capacity.name}
+      </div>
+    )
+  }
+}
+
+class IndicatorGroup extends Component {
+  render() {
+    return (
+      <div className={styles.indicatorGroup}>
+        <div className={styles.indicatorTitle}>
+          {this.props.indicator.name}
+        </div>
+        <div className={`${styles.capacities} ${this.props.indicator.capacities.length === 0 ? styles.capacitiesEmpty: null}`}>
+        {
+          this.props.indicator.capacities.map((capacity) =>
+            <Capacity capacity={capacity} />
+          )
+        }
+        </div>
+      </div>
+    )
+  }
+}
+
+export class TableOfContents extends Component {
+  render() {
+    return (
+      <div className={styles.tableOfContents}>
+      {
+        this.props.entries.map((indicator) =>
+          <IndicatorGroup indicator={indicator}/>
+        )
+      }
+      </div>
+    )
+  }
+}
