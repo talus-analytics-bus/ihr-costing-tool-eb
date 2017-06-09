c7e8b48ec556422e467060f7ba1a79cd43c492eb
Nicolai Vasquez <nrvasquez@gmail.com>
2017-06-09 13:49:04 +0100

    add colors to indicator levels

--- src/scenes/Assessment/components/IndicatorLevels/IndicatorLevel.js
+++ src/scenes/Assessment/components/IndicatorLevels/IndicatorLevel.js
@@ -3,13 +3,25 @@ import styles from './IndicatorLevels.scss';
 import Chip from 'material-ui/Chip';
 import Avatar from 'material-ui/Avatar';
 
+const styleMap = [
+  'danger',
+  'warning',
+  'warning',
+  'success',
+  'success',
+];
+
 export class IndicatorLevel extends Component {
+  levelStyle = (index) => styles[styleMap[index] || 'none'];
+
   render() {
     return (
       <Chip
-        className={styles.indicatorLevel}
+        className={`${styles.indicatorLevel} ${this.levelStyle(this.props.index)}`}
       >
-        <Avatar>{this.props.index}</Avatar>
+        <Avatar
+          className={styles.indicatorLevelNumber}
+        >{this.props.index}</Avatar>
         {this.props.label}
       </Chip>
     );
